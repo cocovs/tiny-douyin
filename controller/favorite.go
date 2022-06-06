3fed4da4f3163770e6814d0459654a735598ca47
xuni <1029805801@qq.com>
2022-06-07 02:27:00 +0800

    点赞列表实现

--- controller/favorite.go
+++ controller/favorite.go
@@ -2,11 +2,10 @@ package controller
 
 //收藏
 import (
-	"context"
 	"github.com/gin-gonic/gin"
-	"github.com/go-redis/redis/v8"
+	"github.com/jinzhu/gorm"
 	"net/http"
-	"time"
+	"strconv"
 )
 
 // FavoriteAction 点赞操作
@@ -15,20 +14,19 @@ func FavoriteAction(c *gin.Context) {
 	videoId := c.PostForm("video_id")
 	actionType := c.PostForm("action_type")
 
-	var user User
-	DB.Where("token = ?", token).First(&user)
-
-	if (user == User{}) {
+	userId := isUserExists(token)
+	if userId == -1 {
 		c.JSON(http.StatusOK, Response{StatusCode: 1, StatusMsg: "User doesn't exist"})
 	}
 
-	var video Video
-	DB.Where("video_id = ?", videoId).First(&video)
+	key := "video:liked:" + videoId
 
-	if video.IsFavorite = actionType == "1"; video.IsFavorite {
-		video.FavoriteCount++
+	if actionType == "1" {
+		DB.Model(User{}).Where("id = ?", videoId).UpdateColumn("favorite_count", gorm.Expr("favorite_count + 1"))
+		RDB.SAdd(ctx, key, userId)
 	} else {
-		video.FavoriteCount--
+		DB.Model(User{}).Where("id = ?", videoId).UpdateColumn("favorite_count", gorm.Expr("favorite_count - 1"))
+		RDB.SRem(ctx, key, userId)
 	}
 	c.JSON(http.StatusOK, Response{StatusCode: 0})
 
@@ -40,28 +38,49 @@ func FavoriteAction(c *gin.Context) {
 
 }
 
-// FavoriteList all users have same favorite video list
 func FavoriteList(c *gin.Context) {
-	var VideoList []Video
+	token := c.Query("token")
+	userId, _ := strconv.ParseInt(c.Query("user_id"), 10, 64)
+
+	// 查询用户是否存在
+	if isUserExists(token) == -1 {
+		c.JSON(http.StatusOK, Response{StatusCode: 1, StatusMsg: "User不存在"})
+	}
+
+	var video []Video
+	result := DB.Find(&video)
+	var videoList []Video
+	// 填充videoList
+	for i := int64(0); i < result.RowsAffected; i++ {
+		if isVideoLike(video[i], userId) {
+			videoList = append(videoList, video[i])
+		}
+	}
+
 	c.JSON(http.StatusOK, VideoListResponse{
 		Response: Response{
 			StatusCode: 0,
 		},
-		VideoList: VideoList,
+		VideoList: videoList,
 	})
 }
 
-func initRedis(redisDb *redis.Client) (err error) {
-	redisDb = redis.NewClient(&redis.Options{
-		Addr:     "localhost:6379",
-		Password: "123456",
-		DB:       0,
-	})
-	timeoutCtx, cancelFunc := context.WithTimeout(context.Background(), time.Second*5)
-	defer cancelFunc()
-	_, err = redisDb.Ping(timeoutCtx).Result()
-	if err != nil {
-		return err
+// 根据token查询用户是否存在
+func isUserExists(token string) int64 {
+	var user User
+	DB.Where("token = ?", token).First(&user)
+
+	if (user == User{}) {
+		return -1
 	}
-	return nil
+	return user.Id
+}
+
+// 查询video是否被当前user点赞并设置isFavorite
+func isVideoLike(video Video, userId int64) bool {
+	videoId := strconv.FormatInt(video.Id, 10)
+	key := "video:liked:" + videoId
+	isMember := RDB.SIsMember(ctx, key, userId)
+	DB.Where("videoId = ?", videoId).Update("is_favorite", isMember.Val())
+	return isMember.Val()
 }
